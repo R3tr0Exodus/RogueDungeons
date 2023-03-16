a764c0a60676844ef712263502fefbc872429719
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2023-03-16 17:59:33 +0100

    plz work

--- src/main.py
+++ src/main.py
@@ -2,31 +2,41 @@
 import pygame
 import GameObject
 from WindowRenderer import WindowRenderer
-from Utility import check_button_press
+from Utility import check_button_press, Layers, update_gameobjects
 
 
-def test_func():
-    print('This was printed from a test')
+def open_inv():
+    pass
+
+
+def start_attack():
+    pass
+
+
+def continue_dungeon():
+    pass
+
+
+def use_item():
+    pass
 
 
 if __name__ == "__main__":
     window = WindowRenderer(50, 50)
     window.set_background_color(255, 0, 255)
 
-    Jeffrey = GameObject.Player(50, 0, pygame.Rect(50, 50, 500, 500))
+    Jeffrey = GameObject.Player(50, 0, pygame.Rect(50, 50, 500, 500), Layers.ENTITIES)
 
     # Create UI buttons
-    buttons = {
-        'InvButton': GameObject.UiButton(test_func, pygame.Rect(750, 550, 100, 100)),
-        'AttackButton': GameObject.UiButton(test_func(), pygame.Rect(200, 550, 450, 100))
+    buttons = [
+        GameObject.UiButton(open_inv, pygame.Rect(750, 550, 100, 100), Layers.UI),         # Inventory button
+        GameObject.UiButton(start_attack, pygame.Rect(200, 550, 450, 100), Layers.UI)      # Attack button
 
-    }
+    ]
 
     running = True
     while running:
-        window.draw_game_object(Jeffrey.sprite, Jeffrey.rect)
-        window.draw_game_object(buttons['InvButton'].sprite, buttons['InvButton'].rect)
-        window.draw_game_object(buttons['AttackButton'].sprite, buttons['AttackButton'].rect)
+        update_gameobjects([Jeffrey, buttons], window=window)
         window.update()
         for event in pygame.event.get():
             # Check for QUIT event
@@ -42,7 +52,7 @@ if __name__ == "__main__":
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    check_button_press(buttons, mousePos=pygame.mouse.get_pos())
+                    check_button_press(buttons, pygame.mouse.get_pos())
 
 
 pygame.quit()
