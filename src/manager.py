97ad21109f7f3e6e4b994bf94811ff690d103783
AtleSkaanes <118199811+AtleSkaanes@users.noreply.github.com>
2023-03-17 08:24:41 +0100

    Set up basic healthbar

--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,6 @@
 import GameObject
+from WindowRenderer import WindowRenderer
+import pygame
 
 
 class Room:
@@ -23,11 +25,21 @@ class TurnManager:
     turnIndex: int = 0
     player: GameObject.Player = None
     enemies: list[GameObject.Entity] = None
+    __fullHeartSprite = pygame.image.load("../sprites/Heart_sprite.png")
+    __emptyHeartSprite = pygame.image.load("../sprites/EmptyHeart_sprite.png")
 
-    def __init__(self, player: GameObject.Player, enemy_list: list[GameObject.Entity]):
+    __screen: WindowRenderer = None
+
+    def __init__(self, player: GameObject.Player, enemy_list: list[GameObject.Entity], screen: WindowRenderer):
         self.player = player
         self.enemies = enemy_list
+        self.__screen = screen
 
     def next_turn(self):
         self.turnIndex += 1
+
+    def draw_hp(self):
+        for i in range(self.player.health):
+            self.__screen.draw_game_object(self.__fullHeartSprite, pygame.Rect(50 * i, 50, 50, 50))
+
 
