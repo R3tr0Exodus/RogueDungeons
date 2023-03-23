71e6ce02d810a70ed4185cecf5df299f9b8d0570
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2023-03-23 11:53:44 +0100

    added an Enemy class, also changed Layer and sprite to be read only properties of gameobject

--- src/GameObject.py
+++ src/GameObject.py
@@ -4,28 +4,38 @@ import pygame
 class GameObject(object):
     instancelist = []  # keep track of all gameobjects
 
-    def __init__(self, xPos, yPos, scale, layer: int, spritePath="../sprites/Error_Placeholder.png", visible: bool=True):
-        self.sprite = pygame.image.load(spritePath)
-        self.layer = layer
+    def __init__(self, xPos, yPos, scale, layer: int, spritePath="../sprites/Error_Placeholder.png",
+                 visible: bool = True):
+        self._sprite = pygame.image.load(spritePath)
+        self._layer = layer
         self.visible = visible
 
-        self.sprite = pygame.transform.scale(self.sprite,
-                                             (self.sprite.get_rect().width * scale,
-                                              self.sprite.get_rect().height * scale))
+        self._sprite = pygame.transform.scale(self._sprite,
+                                             (self._sprite.get_rect().width * scale,
+                                              self._sprite.get_rect().height * scale))
 
-        self.rect = pygame.Rect(xPos, yPos, self.sprite.get_rect().width, self.sprite.get_rect().height)
+        self.rect = pygame.Rect(xPos, yPos, self._sprite.get_rect().width, self._sprite.get_rect().height)
 
         GameObject.instancelist.append(self)
         GameObject.instancelist.sort(key=lambda gameOBJ: gameOBJ.layer, reverse=True)
 
-    def __del__(self):
-        GameObject.instancelist.remove(self)
+    @property
+    def sprite(self):
+        return self._sprite
+
+    @property
+    def layer(self):
+        return self._layer
+
 
     def update(self):
         pass
 
     def start(self):
         pass
+
+    def remove(self):
+        GameObject.instancelist.remove(self)
 
 
 class Item(GameObject):
@@ -39,7 +49,8 @@ class Buff(GameObject):
 
 
 class Entity(GameObject):
-    def __init__(self, baseHealth, baseDmg, xPos, yPos, scale, layer: int, spritePath="../sprites/Jerry_sprite.png", visible=True):
+    def __init__(self, baseHealth, baseDmg, xPos, yPos, scale, layer: int, spritePath="../sprites/Jerry_sprite.png",
+                 visible=True):
         super().__init__(xPos, yPos, scale, layer, spritePath, visible)
         self.health = self.baseHealth = baseHealth
         self.dmg = self.baseDmg = baseDmg
@@ -50,11 +61,25 @@ class Entity(GameObject):
             self.die()
 
     def die(self):
-        print("ouchie!")
+        print('auuuu')
+        self.remove()
+
+
+class Enemy(Entity):
+
+    def __init__(self, baseHealth, baseDmg, xPos, yPos, scale, layer: int, spritePath="../sprites/Jerry_sprite.png",
+                 debuff: Buff = Buff(), visible=True):
+        super().__init__(baseHealth, baseDmg, xPos, yPos, scale, layer, spritePath, visible)
+        self.__debuff: Buff = debuff
+
+    @property
+    def debuff(self):
+        return self.__debuff
 
 
 class Player(Entity):
-    def __init__(self, baseHealth, baseDmg, xPos, yPos, scale, layer: int, spritePath="../sprites/Jerry_sprite.png", visible=True):
+    def __init__(self, baseHealth, baseDmg, xPos, yPos, scale, layer: int, spritePath="../sprites/Jerry_sprite.png",
+                 visible=True):
         super().__init__(baseHealth, baseDmg, xPos, yPos, scale, layer, spritePath, visible)
 
         self.__inventory: list[Item] = []
